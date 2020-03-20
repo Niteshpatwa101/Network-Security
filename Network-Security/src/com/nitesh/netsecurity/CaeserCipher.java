23b3aaef87d2d47e0903115e1fde0001bb0561c7
Nitesh Patwa <patwanitesh1997@gmail.com>
2020-03-20 22:11:20 +0530

    Caeser Cipher added

---
+++ Network-Security/src/com/nitesh/netsecurity/CaeserCipher.java
@@ -0,0 +1,43 @@
+package com.nitesh.netsecurity;
+
+import java.util.Scanner;
+
+/*
+ * An example of Caeser Ciphering
+ * It takes an input(in upper case) and key from user
+ * And ciphers and deciphers data
+ */
+
+public class CaeserCipher {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("ENTER TEXT : ");
+		String plainText = sc.nextLine();
+		System.out.println("ENTER KEY  : ");
+		int key = sc.nextInt();
+		String cipherText = doEncrypt(plainText, key);
+		System.out.println("cipherText : " + cipherText);
+		System.out.println("plainText  : " + doDecrypt(cipherText, key));
+	}
+
+	static String doEncrypt(String plainText, int key) {
+		String temp = "";
+		int offset = 65;
+		
+		for(char ch : plainText.toCharArray()) {
+			int num = ch - offset + key;
+			if(num >= 26) {
+				num -= 26;
+			}
+			temp = temp + (char)(num + offset);
+		}
+		
+		return temp;
+	}
+	
+	static String doDecrypt(String cipherText, int key) {
+		return doEncrypt(cipherText, 26 - key);
+	}
+}
